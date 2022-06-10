d32fdfdb7d18569958a788eb0766cd041a669a78
Vladimir <57994892+wladik06@users.noreply.github.com>
2022-06-11 00:16:27 +0200

    Main panel setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+	return (
+		<div className="flex w-full p-3 absolute">
+			<Link to="/">
+				<h1 className="text-green-600 text-4xl font-bold cursor-pointer">MOVIEAPP</h1>
+			</Link>
+			<div className="items-center justify-end z-[100] flex w-full absolute">
+				<div className="pr-3">
+					<button className="text-white pr-4">Account</button>
+
+					<button className="bg-green-600 px-6 py-2 rounded cursor-pointer text-white">
+						Logout
+					</button>
+				</div>
+
+				<div>
+					<button className="text-white pr-4">Sign In</button>
+
+					<button className="bg-green-600 px-6 py-2 rounded cursor-pointer text-white">
+						Sign Up
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
