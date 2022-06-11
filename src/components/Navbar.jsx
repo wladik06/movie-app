6000a1d34a0dbdbc16d0271e4bb0ffd784c34178
Vladimir <57994892+wladik06@users.noreply.github.com>
2022-06-12 00:22:17 +0200

    Created registration form

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,23 +3,25 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
 	return (
-		<div className="flex items-center justify-between w-full p-3 absolute z-[100]">
-			<Link to="/">
-				<h1 className="text-green-600 text-4xl font-bold cursor-pointer">
-					MOVIEAPP
-				</h1>
-			</Link>
-			<div>
-				<Link to="/login">
-					<button className="text-white pr-4">Log In</button>
+		<>
+			<div className="flex items-center justify-between w-full p-3 absolute z-[100]">
+				<Link to="/">
+					<h1 className="text-green-600 text-4xl font-bold cursor-pointer">
+						MOVIEAPP
+					</h1>
 				</Link>
-				<Link to="/register">
-					<button className="bg-green-600 px-6 py-2 rounded cursor-pointer text-white">
-						Register
-					</button>
-				</Link>
+				<div>
+					<Link to="/login">
+						<button className="text-white pr-4">Log In</button>
+					</Link>
+					<Link to="/register">
+						<button className="bg-green-600 px-6 py-2 rounded cursor-pointer text-white">
+							Register
+						</button>
+					</Link>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+	// styling registration form
+	return (
+		<>
+			<div className="w-full h-screen">
+				<img
+					className="hidden sm:block absolute w-full h-full object-cover"
+					src="https://images.unsplash.com/photo-1631148611744-06fdccf663af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
+					alt="/"
+				/>
+				<div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
+				<div className="fixed w-full px-4 py-24 z-50">
+					<div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+						<div className="max-w-[320px] mx-auto py-16">
+							<h1 className="text-3xl font-bold">Register</h1>
+							<form className="w-full flex flex-col py-4">
+								<input
+									className="p-3 my-2 bg-gray-700 rouded"
+									type="email"
+									placeholder="Email"
+									autoComplete="email"
+								/>
+								<input
+									className="p-3 my-2 bg-gray-700 rouded"
+									type="password"
+									placeholder="Password"
+									autoComplete="current-password"
+								/>
+								<button className="bg-green-600 py-3 my-6 rounded font-bold">
+									Sign Up
+								</button>
+								<p className="py-8">
+									<span className="text-gray-600">
+										Already have an account?
+									</span>
+									<Link to="/login">Log in</Link>
+								</p>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
 
-export default Register
+export default Register;
